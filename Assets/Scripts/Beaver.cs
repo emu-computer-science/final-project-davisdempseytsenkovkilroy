4ca4ed87cc64ce8bc03976a6ece9bd6c49f17f40
mtsenkov <matthew.tsenkov@gmail.com>
2020-03-31 22:56:47 -0400

    very basic WASD movement + gas can pick up working

---
+++ Assets/Scripts/Beaver.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beaver : MonoBehaviour
+{
+    /*
+    public float speed = 1f;
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        transform.position = pos;
+    }
+    */
+    [SerializeField] float speed = 10;
+    public Rigidbody2D rb;
+
+    private Vector2 moveVelocity;
+
+    public void Update()
+    {
+        /*
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 tempVect = new Vector3(h, v, 0);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        rb.MovePosition(rb.transform.position + tempVect);
+        rb.AddForce(rb.transform.position + tempVect);
+        */
+
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "GasCan")
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/GasCan.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GasCan : MonoBehaviour
+{
+    [SerializeField] private Text pickUpText;
+
+    private bool pickUpAllowed;
+
+    private void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.F))
+        {
+            PickUp();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Beaver")
+        {
+            Debug.Log("Collision with beaver detected.");
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Beaver")
+        {
+            pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
+        }
+    }
+
+    private void PickUp()
+    {
+        Destroy(gameObject);
+    }
+}
