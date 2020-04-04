62c0f1847cd6f1948f67ca1b3cddafdb1ef601e4
Dillon Kilroy <dillonkilroy@gmail.com>
2020-04-04 18:10:14 -0400

    Got the AI to move on a created path and the speed of the path movement working.

---
+++ Assets/Scripts/EnemyFollowPath.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowPath : MonoBehaviour
+{
+    public enum MovementTypes
+    {
+        MoveTowards, LerpTowards
+    }
+
+    public MovementTypes type = MovementTypes.MoveTowards;
+    public EnemyMovement AIPath;
+    public float moveSpeed = 1f;
+    public float maxDistanceFromPoint = .1f;
+
+    private IEnumerator<Transform> pointInThePath;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointInThePath = AIPath.GetTheNextPoint();
+
+        pointInThePath.MoveNext();
+
+        transform.position = pointInThePath.Current.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (type == MovementTypes.MoveTowards)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pointInThePath.Current.position, Time.deltaTime * moveSpeed);
+        }
+        else if (type == MovementTypes.LerpTowards)
+        {
+            transform.position = Vector3.Lerp(transform.position, pointInThePath.Current.position, Time.deltaTime * moveSpeed);
+        }
+
+        float dSquared = (transform.position - pointInThePath.Current.position).sqrMagnitude;
+        if (dSquared < maxDistanceFromPoint * maxDistanceFromPoint)
+        {
+            pointInThePath.MoveNext();
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public int moveDirection = 1;
+    public int moveToPointInPath = 0;
+    public Transform[] pathArr;
+
+    public void OnDrawGizmos()
+    {
+        if (pathArr == null || pathArr.Length < 2)
+        {
+            return;
+        }
+
+        for (int i = 1; i < pathArr.Length; i++)
+        {
+            Gizmos.DrawLine(pathArr[i - 1].position, pathArr[i].position);
+        }
+
+        Gizmos.DrawLine(pathArr[0].position, pathArr[pathArr.Length - 1].position);
+
+    }
+
+    public IEnumerator<Transform> GetTheNextPoint()
+    {
+        if (pathArr == null || pathArr.Length < 1)
+        {
+            yield break;
+        }
+
+        while (true)
+        {
+            yield return pathArr[moveToPointInPath];
+
+            if (pathArr.Length == 1)
+            {
+                continue;
+            }
+
+
+
+            moveToPointInPath += moveDirection;
+
+            if (moveToPointInPath >= pathArr.Length)
+            {
+                moveToPointInPath = 0;
+            }
+
+            if (moveToPointInPath < 0)
+            {
+                moveToPointInPath = pathArr.Length - 1;
+            }
+        }
+    }
+}
