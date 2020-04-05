5cc82172b48f6ad68ce5582915784bcf25aae484
mtsenkov <matthew.tsenkov@gmail.com>
2020-04-04 23:03:00 -0400

    merging master and DillonEdit

---
+++ Assets/Scripts/AIDetectionField.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDetectionField : MonoBehaviour
+{
+    public Transform player;
+    public float maxAngle;
+    public float maxRadius;
+
+    
+
+}
