73dd41e12b638bf9efd7af45c0ae04fd1f7bedd4
mtsenkov <matthew.tsenkov@gmail.com>
2020-04-04 21:28:22 -0400

    Added follow cam with smoothing

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FollowCam : MonoBehaviour
+{
+    private Func<Vector3> GetCameraFollowPositionFunc;
+
+    public void Setup(Func<Vector3> GetCameraFollowPositionFunc)
+    {
+        this.GetCameraFollowPositionFunc = GetCameraFollowPositionFunc;
+    }
+    private void Update()
+    {
+        Vector3 cameraFollowPosition = GetCameraFollowPositionFunc();
+        cameraFollowPosition.z = transform.position.z;
+
+        Vector3 cameraMoveDirection = (cameraFollowPosition - transform.position).normalized;       //Smoothing the camera
+        float distance = Vector3.Distance(cameraFollowPosition, transform.position);
+        float cameraMoveSpeed = 1f;
+
+        transform.position = transform.position + cameraMoveDirection * distance * cameraMoveSpeed * Time.deltaTime;
+    }
+}
