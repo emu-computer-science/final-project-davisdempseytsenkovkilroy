a8d43d0724a88ede0a2bf39ee13ee15dd0d150ff
Leah Dempsey <leahdemp14@gmail.com>
2020-04-06 21:05:07 -0400

    changed some things with item spawn

--- Assets/Scripts/SpawnItems.cs
+++ Assets/Scripts/SpawnItems.cs
@@ -6,35 +6,60 @@ public class SpawnItems : MonoBehaviour
 {
     //public GameObject item;
     [Header("Set in Inpsector")]
-    public Vector2 itemPosMin = new Vector2(-50, -5); 
+    public Vector2 itemPosMin = new Vector2(-50, -5);
     public Vector2 itemPosMax = new Vector2(150, 100);
     private int xPos;
     private int yPos;
     private int itemCount = 0;
     public int numItems;
     public GameObject GasCanPrefab;
+    public float itemCheckRadius = 3f;
 
     private void Start()
     {
-        Spawn();    
+        Spawn();
     }
 
     void Spawn()
     {
         GameObject item;
         GameObject anchor = GameObject.Find("SpawnItems");
+        
         //GasCanPrefab = GameObject.Find("GasCan");
 
         while (itemCount < numItems)
         {
-            item = Instantiate<GameObject>(GasCanPrefab);
+            bool validPosition = false;
+            //item = Instantiate<GameObject>(GasCanPrefab);
             Vector2 itemPos = Vector2.zero;
-            itemPos.x = Random.Range(itemPosMin.x, itemPosMax.x);
-            itemPos.y = Random.Range(itemPosMin.y, itemPosMax.y);
-            item.transform.position = itemPos;
-            //Instantiate(item, new Vector2(xPos, yPos), Quaternion.identity);
-            item.transform.SetParent(anchor.transform);
-            itemCount += 1;
+            while (!validPosition)
+            {
+                itemPos.x = Random.Range(itemPosMin.x, itemPosMax.x);
+                itemPos.y = Random.Range(itemPosMin.y, itemPosMax.y);
+                Debug.Log("itemPos.x: " + itemPos.x + "itemPos.y" + itemPos.y);
+
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(itemPos, itemCheckRadius);
+                if (colliders.Length > 0)
+                {
+                    Debug.Log(colliders);
+                    Debug.Log("not a valid position");
+                    validPosition = false;
+                    break;
+
+                } else
+                {
+                    validPosition = true;
+                    Debug.Log("valid position");
+                    item = Instantiate<GameObject>(GasCanPrefab);
+                    Debug.Log(itemPos);
+                    item.transform.position = itemPos;
+                    //Instantiate(item, new Vector2(xPos, yPos), Quaternion.identity);
+                    item.transform.SetParent(anchor.transform);
+                    itemCount += 1;
+                    break;
+                }       
+            }
+           
         }
     }
 }
