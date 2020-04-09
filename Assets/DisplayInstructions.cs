b08e5f4e00718f52441ae275df960027363c81a4
Leah Dempsey <leahdemp14@gmail.com>
2020-04-08 20:51:02 -0400

    main menu functionality

---
+++ Assets/DisplayInstructions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DisplayInstructions : MonoBehaviour
+{
+    public void Display()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+}
